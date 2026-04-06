c5029a058bd7a08c4be224c71365e28421cb3ede
Harrison Wyatt <n11146362@qut.edu.au>
2026-04-06 11:38:21 +1000

    contact form basic styling

---
+++ Functions/qoute.js
@@ -0,0 +1,27 @@
+export async function onRequestPost({ request, env }) {
+  const formData = await request.formData();
+
+  const data = {
+    name: formData.get('name'),
+    email: formData.get('email'),
+    mobil: formData.get('mobile'),
+    suburb: formData.get('suburb'),
+    message: formData.get('message')
+  };
+
+  await fetch('https://api.resend.com/emails', {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${env.RESEND_API_KEY}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      from: 'onboarding@resend.dev',
+      to: 'Cahill1807@gmail.com',
+      subject: 'New Quote Request',
+      html: `<p>${data.name} (${data.email}) (${data.mobile})</p><p>${data.suburb}</p><p>${data.message}</p>`
+    })
+  });
+
+  return new Response('OK');
+}
